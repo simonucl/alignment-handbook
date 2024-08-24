00581c0d9697289b03068fe8fc715a00898560cf
simonucl <simon011130@gmail.com>
2024-08-24 18:59:58 +0800

    again

--- src/alignment/data.py
+++ src/alignment/data.py
@@ -96,8 +96,8 @@ def apply_chat_template(
 
             example["text_prompt"] = tokenizer.apply_chat_template(prompt_messages, tokenize=False)
             if isinstance(tokenizer, CohereTokenizerFast):
-                example["text_chosen"] = f"'<|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>'  + {example['chosen'][-1]['content'].strip()} + '<|END_OF_TURN_TOKEN|>'"
-                example["text_rejected"] = f"'<|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>'  + {example['rejected'][-1]['content'].strip()} + '<|END_OF_TURN_TOKEN|>'"
+                example["text_chosen"] = '<|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>'  + example['chosen'][-1]['content'].strip() + '<|END_OF_TURN_TOKEN|>'
+                example["text_rejected"] = '<|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>' + example['rejected'][-1]['content'].strip() + '<|END_OF_TURN_TOKEN|>'
             else:
                 example["text_chosen"] = tokenizer.apply_chat_template(chosen_messages, tokenize=False)
                 example["text_rejected"] = tokenizer.apply_chat_template(rejected_messages, tokenize=False)
