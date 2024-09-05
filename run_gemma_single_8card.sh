8e1b6bf9af5843ed3354a404a7b9009b1a8bd374
simonucl <simon011130@gmail.com>
2024-09-05 14:50:45 -0400

    add gemma run

---
+++ run_gemma_single_8card.sh
@@ -0,0 +1,3 @@
+DS_SKIP_CUDA_CHECK=1 ACCELERATE_LOG_LEVEL=info nohup accelerate launch \
+    --config_file recipes/accelerate_configs/deepspeed_zero3_8card.yaml \
+    scripts/run_dpo.py recipes/gemma2-9b-single-judge.yaml  > logs/dpo.yaml 2>&1 &
